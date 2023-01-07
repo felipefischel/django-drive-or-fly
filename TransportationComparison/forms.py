c218dff8bb49dd391be3f1547f05274eb99f85e4
robertomr100 <robertomr100@gmail.com>
2023-01-07 22:43:27 +0100

    changed placeholders and labels

--- TransportationComparison/forms.py
+++ TransportationComparison/forms.py
@@ -3,9 +3,9 @@ from django.conf import settings
 
 
 class TripForm(forms.Form):
-    starting_destination = forms.CharField()
-    final_destination = forms.CharField()
+    starting_destination = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Where From?'}))
+    final_destination = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'Where To?'}))
     #date_start = forms.DateField(widget = forms.SelectDateWidget)
     #date_end = forms.DateField(widget = forms.SelectDateWidget)
     date_start = forms.DateField()
-    date_end = forms.DateField()
+    #date_end = forms.DateField()
