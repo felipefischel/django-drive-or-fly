5d605fad8b0e2f41f8b4a2e7cf336672d4660624
Felipe Fischel <felipefischel@gmail.com>
2023-01-21 19:46:33 +0100

    small changes to results page

--- TransportationComparison/services/gasApiService.py
+++ TransportationComparison/services/gasApiService.py
@@ -7,19 +7,18 @@ from django.conf import settings
 
 #returns $ per gallon
 def getGasPricesByUSState():
-
+    
   conn = http.client.HTTPSConnection("api.collectapi.com")
-  
+
   headers = {
       'content-type': "application/json",
       'authorization': settings.COLLECT_API_KEY
   }
-  
+
   conn.request("GET", "/gasPrice/stateUsaPrice?state=WA", headers=headers)
-  
+
   res = conn.getresponse()
   data = res.read()
   data_as_json = json.loads(data)
 
   return data_as_json['result']['state']['gasoline']
-  
