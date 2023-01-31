8bfb4484e84fc13be7a736659bffd524e078b8f9
Felipe Fischel <felipefischel@gmail.com>
2023-01-30 20:30:42 -0600

    created about page and change urls

--- TransportationComparison/urls.py
+++ TransportationComparison/urls.py
@@ -6,6 +6,7 @@ app_name = 'comparison'
 
 urlpatterns =[
   path('',views.Index, name='index'),
+  path('about/',views.About, name='about'),
   path('result/<int:trip_output_id>', views.Result, name='result'),
   path('compare/',views.Compare,name='compare'),
   path('autocomplete/', views.autocomplete, name="autocomplete")
